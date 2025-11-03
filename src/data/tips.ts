2db9b365948ed33ba120ca30a579f0a98c024d7f
Serhii Staryk <serhii.staryk@gmail.com>
2025-11-03 23:09:49 +0200

    fix: correct category and language properties for tips

--- src/data/tips.ts
+++ src/data/tips.ts
@@ -1,6 +1,8 @@
 export const tips = [
   {
     title: 'String literal union pattern',
+    category: 'Type Script',
+    language: 'typescript',
     description: 'Creating a discriminated string union type from a readonly constant object.',
     code: `export const OrderStatus = {
  Open: 'Open',
@@ -10,7 +12,36 @@ export const tips = [
 } as const;
 
 export type OrderStatus = (typeof OrderStatus)[keyof typeof OrderStatus]`,
-    category: 'Type Script',
-    language: 'typescript',
+  },
+
+  {
+    title: 'Responsive Image Styling',
+    category: 'CSS',
+    language: 'css',
+    description: `This CSS snippet ensures that images scale proportionally within their container. 
+      The max-width: 100% property prevents images from exceeding the container's width, 
+      while height: auto maintains the original aspect ratio.`,
+    code: `img { 
+ max-width: 100%; 
+ height: auto 
+}`,
+  },
+
+  {
+    title: 'Global Box-Sizing Reset',
+    category: 'CSS',
+    language: 'css',
+    description: `his CSS snippet sets box-sizing: border-box globally, ensuring that padding and borders are included 
+within an element's total width and height. The universal selector * and its pseudo-elements *:before and *:after 
+inherit this behavior, which simplifies layout calculations and prevents unexpected sizing issues.`,
+    code: `html {
+ box-sizing: border-box; 
+}
+
+*,
+*:before,
+*:after {
+ box-sizing: inherit; 
+}`,
   },
 ];
